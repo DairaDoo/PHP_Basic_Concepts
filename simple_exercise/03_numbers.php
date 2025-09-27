a7626b452e4d5ca5a2f665e27aad5e357a7eab6e
DairanDoo <your_email@example.com>
2025-09-27 17:00:02 -0400

    operaciones basicas con números.

---
+++ simple_exercise/03_numbers.php
@@ -0,0 +1,15 @@
+<?php
+
+$x = 10;
+$y = 20;
+
+echo $x + $y . "<br>"; // addition
+echo $x - $y . "<br>"; // subtraction
+echo $x * $y . "<br>"; // multiplication
+echo $y / $x . "<br>"; // division
+echo $y % $x . "<br>"; // modulus
+echo $x ** 2 . "<br>"; // exponentiation
+
+echo min($x, $y) . "<br>"; // minimum value
+echo max($x, $y) . "<br>"; // maximum value
+
